651d46a9be5ab8db0efc372a09266808b58338f1
Martin Dhenu <martin.dhenu@chefkoch.de>
2020-10-28 17:11:08 +0100

    Feature: implement reactjs

---
+++ assets/js/react/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ListCreator from './components/ListCreator';
+
+ReactDOM.render(
+    <React.Fragment>
+        <ListCreator></ListCreator>
+    </React.Fragment>,
+    document.getElementById('root')
+);
+
+
+
---
+++ assets/js/react/components/ListCreator.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class ListCreator extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>List creator component</h1>
+                <h2>Create your LIst here</h2>
+            </div>
+
+        )
+    }
+}
+
+export default ListCreator;
